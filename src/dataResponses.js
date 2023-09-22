5a0b1f23d9966dc4313f3f4c0d3f24bf5764b06b
Brian Egan (RIT Student) <bpe4955@rit.edu>
2023-09-22 16:31:06 -0400

    All codes are handled, just need to add URL search support

---
+++ src/dataResponses.js
@@ -0,0 +1,189 @@
+// function to send a json object
+const respond = (request, response, status, object, type = '*/*') => {
+  response.writeHead(status, { 'Content-Type': type });
+
+  if (object) { response.write(object); }
+
+  // Send the response to the client
+  response.end();
+};
+
+// function to show a success status code
+const success = (request, response, params, acceptedTypes) => {
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    const responseObj = {
+      message: 'This is a successful response',
+    };
+    // send our json with a success status code
+    return respond(request, response, 200, JSON.stringify(responseObj), 'application/json');
+  }
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseObj = '<response> <message>This is a successful response</message> </response>';
+    return respond(request, response, 200, responseObj, 'text/xml');
+  }
+
+  // Not requesting JSON or XML
+  return respond(request, response, 400, 'Server can only handle JSON or XML requests at this URI.', 'text/plain');
+};
+
+// function to show a bad request without the correct parameters
+const badRequest = (request, response, params, acceptedTypes) => {
+  let responseObj;
+
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    responseObj = {
+      message: 'This request has the required parameters',
+    };
+    // if the request does not contain a valid=true query parameter
+    if (!params.valid || params.valid !== 'true') {
+      // set our error message
+      responseObj.message = 'Missing valid query parameter set to true';
+      // give the error a consistent id
+      responseObj.id = 'badRequest';
+      // return our json with a 400 bad request code
+      return respond(request, response, 400, JSON.stringify(responseObj), 'application/json');
+    }
+    responseObj = JSON.stringify(responseObj);
+  } else if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response> <message>This request has the required parameters</message> </response>';
+    // if the request does not contain a valid=true query parameter
+    if (!params.valid || params.valid !== 'true') {
+      responseObj = '<response> <message>Missing valid query parameter set to true</message> <id>badRequest</id> </response>';
+      // return our json with a 400 bad request code
+      return respond(request, response, 400, responseObj, 'text/xml');
+    }
+  }
+
+  // if the parameter is here, send json with a success status code
+  if (responseObj) { return respond(request, response, 200, responseObj, acceptedTypes[0]); }
+
+  // Not requesting JSON or XML
+  return respond(request, response, 400, 'Server can only handle JSON or XML requests at this URI.', 'text/plain');
+};
+
+const unauthorized = (request, response, params, acceptedTypes) => {
+  let responseObj;
+
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    responseObj = {
+      message: 'User is logged in and can view content',
+    };
+    // if the request does not contain a valid=true query parameter
+    if (!params.loggedIn || params.loggedIn !== 'true') {
+      // set our error message
+      responseObj.message = 'Missing loggedIn query parameter set to true';
+      // give the error a consistent id
+      responseObj.id = 'unauthorized';
+      // return our json with a 401 unauthorized code
+      return respond(request, response, 401, JSON.stringify(responseObj), 'application/json');
+    }
+    responseObj = JSON.stringify(responseObj);
+  } else if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response> <message>User is logged in and can view content</message> </response>';
+    // if the request does not contain a valid=true query parameter
+    if (!params.loggedIn || params.loggedIn !== 'true') {
+      responseObj = '<response> <message>Missing loggedIn query parameter set to true</message> <id>badRequest</id> </response>';
+      // return our json with a 401 unauthorized code
+      return respond(request, response, 401, responseObj, 'text/xml');
+    }
+  }
+  // return our json with a 200 success code
+  if (responseObj) { return respond(request, response, 200, responseObj, acceptedTypes[0]); }
+
+  // Not requesting JSON or XML
+  return respond(request, response, 400, 'Server can only handle JSON or XML requests at this URI.', 'text/plain');
+};
+
+const forbidden = (request, response, params, acceptedTypes) => {
+  let responseObj;
+
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    responseObj = {
+      message: 'You cannot view this page.',
+      id: 'forbidden',
+    };
+    responseObj = JSON.stringify(responseObj);
+  } else if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response> <message>You cannot view this page.</message> <id>forbidden</id> </response>';
+  }
+  // return our data with a 403 forbidden error code
+  respond(request, response, 403, responseObj, acceptedTypes[0]);
+};
+
+const internal = (request, response, params, acceptedTypes) => {
+  let responseObj;
+
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    responseObj = {
+      message: 'The server has encountered an error.',
+      id: 'internal',
+    };
+    responseObj = JSON.stringify(responseObj);
+  } else if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response> <message>The server has encountered an error.</message> <id>internal</id> </response>';
+  }
+  // return our data with a 500 internal server error code
+  if (responseObj) { return respond(request, response, 500, responseObj, acceptedTypes[0]); }
+
+  // Not requesting JSON or XML
+  return respond(request, response, 400, 'Server can only handle JSON or XML requests at this URI.', 'text/plain');
+};
+
+const notImplemented = (request, response, params, acceptedTypes) => {
+  let responseObj;
+
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    responseObj = {
+      message: 'The page you are looking for is not yet ready.',
+      id: 'notImplemented',
+    };
+    responseObj = JSON.stringify(responseObj);
+  } else if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response> <message>The page you are looking for is not yet ready.</message> <id>notImplemented</id> </response>';
+  }
+  // return our data with a 501 not implemented error code
+  if (responseObj) { return respond(request, response, 501, responseObj, acceptedTypes[0]); }
+
+  // Not requesting JSON or XML
+  return respond(request, response, 400, 'Server can only handle JSON or XML requests at this URI.', 'text/plain');
+};
+
+// function to show not found error
+const notFound = (request, response, params, acceptedTypes) => {
+  let responseObj;
+  if (!acceptedTypes) {
+    return respond(request, response, 404);
+  }
+
+  if (acceptedTypes[0] === 'application/json') {
+    // message to send
+    responseObj = {
+      message: 'The page you are looking for was not found.',
+      id: 'notFound',
+    };
+    responseObj = JSON.stringify(responseObj);
+  } else if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response> <message>The page you are looking for was not found.</message> <id>notFound</id> </response>';
+  }
+  // return our data with a 404 not found error code
+  return respond(request, response, 404, responseObj, acceptedTypes[0]);
+};
+
+// exports to set functions to public.
+// In this syntax, you can do getIndex:getIndex, but if they
+// are the same name, you can short handle to just getIndex,
+module.exports = {
+  success,
+  badRequest,
+  notFound,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+};
---
+++ src/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const dataHandler = require('./dataResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getPage,
+  '/style.css': htmlHandler.getPage,
+  '/success': dataHandler.success,
+  '/badRequest': dataHandler.badRequest,
+  '/unauthorized': dataHandler.unauthorized,
+  '/forbidden': dataHandler.forbidden,
+  '/internal': dataHandler.internal,
+  '/notImplemented': dataHandler.notImplemented,
+  '/notFound': dataHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  const parsedURL = url.parse(request.url);
+  const params = query.parse(parsedURL.query);
+  const acceptedTypes = request.headers.accept.split(',');
+
+  if (urlStruct[parsedURL.pathname]) {
+    urlStruct[parsedURL.pathname](request, response, params, acceptedTypes);
+  } else {
+    urlStruct['/notFound'](request, response, params);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
